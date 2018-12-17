d6b713c82c107d9570c26db5ef3ec9042e55cf6b
kukubao207 <502914814@qq.com>
2018-12-18 00:44:06 +0800

    answer页面

--- pages/answer/answer.js
+++ pages/answer/answer.js
@@ -28,6 +28,7 @@ Page({
       let url = "user/" + openid + "/watched/user/" +res.data.data.ownerId
       console.log(url)
       util.getData(url).then(function (res) {
+        console.log(res)
         that.setData({
           isWatched: res.data.data,
         })
@@ -51,15 +52,15 @@ Page({
     let url = "/user/" + openid + "/watch/user/" + e.currentTarget.dataset.ownerid
     util.postData(url,{}).then(function(res){
       console.log(res)
-      if(res.data.data.code===200){
-        that.setData({
-          isWatched:true
-        })
-      }else if(res.data.data.code===201){
+      that.setData({
+        isWatched:true
+      })
+      if(res.data.code===201){
         wx.showToast({
           title: '已经关注过啦',
         })
       }
+      console.log('点击关注按钮后,isWatched:'+that.data.isWatched)
     })
   },
   unWatchUser: function (e) {
@@ -69,15 +70,15 @@ Page({
     let url = "/user/" + openid + "/unwatch/user/" + e.currentTarget.dataset.ownerid
     util.postData(url, {}).then(function (res) {
       console.log(res)
-      if (res.data.data.code === 200) {
-        that.setData({
-          isWatched: false
-        })
-      } else if(res.data.data.code===201) {
+      if(res.data.code===201) {
         wx.showToast({
           title: '已经取关注过啦',
         })
       }
+      that.setData({
+        isWatched: false
+      })
+      console.log('取关后:' + that.data.isWatched)
     })
   },
 
