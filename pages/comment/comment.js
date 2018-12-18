b5e7fa68d3d5f5e7ce70d3f11100a615d617573c
AdamKingsley <desperatewilder@gmail.com>
2018-12-18 23:18:08 +0800

    更新中间页面

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -14,7 +14,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    console.log("options:");
+    console.log(options);
     this.setData({
       answerid:options.answerid
     })
--- pages/notify/notify.js
+++ pages/notify/notify.js
@@ -21,21 +21,21 @@ Page({
       follows: [],
       follow_page: 1
     });
-    this.getMoments();
-    this.getFollows();
+    this.getMoments(1, 6);
+    this.getFollows(1, 6);
   },
-  
-  getFollows: function() {
+
+  getFollows: function(page, page_size) {
     let that = this;
     let ownerid = wx.getStorageSync('ownerid');
-    util.getData('answer/' + ownerid + '/watchUserAnswerList/' + that.data.follow_page + '/' + that.data.follow_page_size).then(res => {
+    util.getData('answer/' + ownerid + '/watchUserAnswerList/' + page + '/' + page_size).then(res => {
       console.log(res);
       if (res.data.code === 200) {
         that.setData({
           follows: that.data.follows.concat(res.data.data.content),
-          follow_page: that.data.follow_page + 1,
+          follow_page: page + 1,
         })
-      } else if(res.data.code !== 201){
+      } else if (res.data.code !== 201) {
         wx.showToast({
           title: '获取数据失败！',
           icon: 'fail',
@@ -52,24 +52,33 @@ Page({
   },
 
 
-  navigatorTo: function(e){
+  navigatorToQuestion: function(e) {
     let qid = e.currentTarget.dataset.qid;
     wx.navigateTo({
       url: '../question/question?id=' + qid
     });
   },
 
-  getMoments: function() {
+  navigatorToComment: function(e) {
+    let aid = e.currentTarget.dataset.aid;
+    console.log(aid);
+    wx.navigateTo({
+      url: '../comment/comment?answerid=' + aid
+    });
+  },
+
+  getMoments: function(page, page_size) {
     let that = this;
     let ownerid = wx.getStorageSync('ownerid');
-    util.getData('answer/' + ownerid + '/commentList/' + that.data.follow_page + '/' + that.data.follow_page_size).then(res => {
+    console.log(that.data.follow_page);
+    util.getData('answer/' + ownerid + '/commentList/' + page + '/' + page_size).then(res => {
       console.log(res);
       if (res.data.code === 200) {
         that.setData({
           moments: that.data.moments.concat(res.data.data.content),
-          moment_page: that.data.moment_page + 1,
+          moment_page: page + 1,
         })
-      } else if(res.data.code !== 201) {
+      } else if (res.data.code !== 201) {
         wx.showToast({
           title: '获取数据失败！',
           icon: 'fail',
@@ -88,5 +97,50 @@ Page({
     this.setData({
       currentNavtab: e.currentTarget.dataset.idx
     });
+  },
+
+  onPullDownRefresh: function() {
+    console.log('on Pull Down Refresh !!! ');
+    let that = this
+    // currentNavtab: "0",
+    // let url = "question/list/1/10"
+    if (that.data.currentNavtab == '0') {
+      that.setData({
+        moments: [],
+        moment_page: 1,
+        moment_page_size: 6,
+      });
+      console.log(that.data.moments)
+      console.log(that.data.moment_page)
+      console.log(that.data.moment_page_size)
+      that.getMoments(that.data.moment_page, that.data.moment_page_size);
+    } else if (that.data.currentNavtab == '1') {
+      that.setData({
+        follows: [],
+        follow_page: 1,
+        follow_page_size: 6,
+      })
+      console.log(that.data.follows)
+      console.log(that.data.follow_page)
+      console.log(that.data.follow_page_size)
+      that.getFollows(that.data.follow_page, that.data.follow_page_size);
+    }
+  },
+  onReachBottom: function() {
+    let that = this;
+    console.log('on Reach Bottom !!! ');
+    // currentNavtab: "0",
+    // let url = "question/list/1/10"
+    if (that.data.currentNavtab == '0') {
+      console.log(that.data.moments)
+      console.log(that.data.moment_page)
+      console.log(that.data.moment_page_size)
+      that.getMoments(that.data.moment_page,that.data.moment_page_size);
+    } else if (that.data.currentNavtab == '1') {
+      console.log(that.data.follows)
+      console.log(that.data.follow_page)
+      console.log(that.data.follow_page_size)
+      that.getFollows(that.data.follow_page,that.data.follow_page_size);
+    }
   }
 })
