9ee60bfab9d1cffb77ad2b8937fdcf590e94a373
kukubao207 <502914814@qq.com>
2018-12-20 11:30:36 +0800

    change

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,12 +1,36 @@
 var app=getApp()
+var util = require("../../utils/util.js")
+
 Page({
   data: {
     icon_url:"../../images/ICON.png",
     userInfo:{}
   },
   checkSession(e) {
+    console.log("check session")
     wx.checkSession({
       success: res => {
+        wx.login({
+          success: res =>{
+            console.log(res)
+            let jsonData = {
+              code: res.code,
+            }
+            let url = "user/open";
+            util.postData(url,jsonData).then(res => {
+              console.log(res)
+              wx.setStorage({
+                key: 'openid',
+                data: res.data.data.openid,
+              })
+              wx.setStorage({
+                key: 'ownerid',
+                data: res.data.data.id,
+              })
+            })
+          }
+        })
+        console.log('before switch to index')
         wx.switchTab({
           url: '/pages/index/index',
         })
@@ -19,38 +43,29 @@ Page({
     console.log(e)
     wx.login({
       success: res => {
-        wx.request({
-          url: "https://"+app.globalData.productIp+'/user/login',
-          method: 'post',
-          header: {
-            'content-type': 'application/json'
-          },
-          data: {
-            code: res.code,
-            userInfo: e.detail.userInfo
-          },
-          success: res => {
-            wx.setStorage({
-              key: 'openid',
-              data: res.data.data.openid,
-            })
-            wx.setStorage({
-              key: 'ownerid',
-              data: res.data.data.id,
-            })
-            // wx.setStorageSync('openid', res.data.data.openid)
-            // wx.setStorageSync('ownerid', res.data.data.id)
-            wx.switchTab({
-              url: '/pages/index/index',
-            })
-          }
+        let jsonData={
+          'code':res.code,
+          'userInfo': e.detail.userInfo
+        }
+        let url = "user/login"
+        util.postData(url,jsonData).then(res=>{
+          wx.setStorage({
+            key: 'openid',
+            data: res.data.data.openid,
+          })
+          wx.setStorage({
+            key: 'ownerid',
+            data: res.data.data.id,
+          })
+          // wx.setStorageSync('openid', res.data.data.openid)
+          // wx.setStorageSync('ownerid', res.data.data.id)
+          wx.switchTab({
+            url: '/pages/index/index',
+          })
         })
       }
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     this.checkSession()
   },
